b84bbb59a99a212f88a26e8796f4cc50a6bc7c27
AtharshanKen <atharshan.kennedy@dcmail.ca>
2026-02-15 13:41:24 -0500

    Added a posthog capture to user selection

--- frontend/app.py
+++ frontend/app.py
@@ -83,7 +83,7 @@ posthog.capture(
     properties={  
         '$pathname': '/app',  # The page path  
     }  
-)  
+) 
 # psthg_html ='''
 # <script>
 #/     !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
@@ -267,7 +267,25 @@ with midR[1]:
                         index=None,
                         placeholder="Select...",
                         key="sel_locN")
-        if sel_locN != None: Dest_Forecastig_Data_Get()
+        if sel_locN != None:
+             # Capture event for anonymous user   
+            posthog.capture(  
+                distinct_id=st.session_state['anon_id'],  
+                event='user_input_submitted',  
+                properties={ 
+                    'Origin': sel_org,
+                    'Arrival_Date': sel_Arv_dte,
+                    'Attraction_Category':sel_att_cat,
+                    'Attraction_Type':sel_att_type,
+                    'Crowd_Tolerance':sel_crowd,
+                    'Temp_Preference':sel_temp,
+                    'Destination':sel_locN,
+                    '$process_person_profile': False , # Don't create person profile (cheaper)  
+                    'environment': os.getenv('STREAMLIT_ENV', 'Development')  
+                }  
+            )  
+            posthog.flush() # end capture before page refresh
+            Dest_Forecastig_Data_Get()
 
 with midR[2]:
     # Update figure with new data if Orgin,Avr Time,Dest have been selected
